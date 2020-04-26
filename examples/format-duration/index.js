f7545786ab458e2b76ebe95bcd73a46864e524e8
Fernando Cristiani <fer5cristiani@gmail.com>
2020-04-26 19:23:53 +0200

    Add format-duration optional parameters example

---
+++ examples/format-duration/index.js
@@ -0,0 +1,45 @@
+var format = require('format-duration')
+
+// anything under a second is rounded down to zero
+format(999) // '0:00'
+
+// 1000 milliseconds is a second
+format(1000) // '0:01'
+
+// 1999 rounds down to 0:01
+format(1000 * 2 - 1) // '0:01'
+
+// 60 seconds is a minute
+format(1000 * 60) // '1:00'
+
+// 59 seconds looks like this
+format(1000 * 60 - 1) // '0:59'
+
+// 60 minutes is an hour
+format(1000 * 60 * 60) // '1:00:00'
+
+// 59 minutes and 59 seconds looks like this
+format(1000 * 60 * 60 - 1) // '59:59'
+
+// 24 hours is a day
+format(1000 * 60 * 60 * 24) // '1:00:00:00'
+
+// 23 hours, 59 minutes, and 59 seconds looks like this
+format(1000 * 60 * 60 * 24 - 1) // '23:59:59'
+
+// 365 days looks like this (not bothering with years)
+format(1000 * 60 * 60 * 24 * 365) // '365:00:00:00'
+
+// anything under a second is rounded down to zero
+format(-999) // '0:00'
+
+// 1000 milliseconds is a second
+format(-1000) // '-0:01'
+
+// 365 days looks like this (not bothering with years)
+format(-1000 * 60 * 60 * 24 * 365) // '-365:00:00:00'
+
+// with `leading` option, formatting looks like this
+format(1000 * 60, { leading: true }) // '01:00'
+format(1000 * 60 - 1, { leading: true }) // '00:59'
+format(1000 * 60 * 60, { leading: true }) // '01:00:00'
