d72caddfc9fe667ed7105b09d94f5f7e91538d29
Fernando Cristiani <fer5cristiani@gmail.com>
2020-05-30 12:49:06 +0200

    Add example for contains-path

---
+++ examples/contains-path/index.js
@@ -0,0 +1,18 @@
+var containsPath = require('contains-path');
+
+containsPath('foo/bar', 'foo', {}); //=> true
+containsPath('foo/bar', 'bar', { nocase: false }); //=> true
+containsPath('foo/bar', 'qux'); //=> false
+
+// // returns false for partial matches
+containsPath('foobar', 'foo', { partialMatch: true });  //=> false
+containsPath('foo.bar', 'foo'); //=> false
+containsPath('foo.bar', 'bar'); //=> false
+
+// prefix with "./" to match from beginning of filepath
+containsPath('bar/foo', 'foo');   //=> true
+containsPath('bar/foo', 'foo');   //=> true
+containsPath('bar/foo', './foo'); //=> false
+
+containsPath('bar/foo', './foo');
+containsPath('foo.bar', 'otherThing');
