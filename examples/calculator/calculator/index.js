fcb0d4ef59110b03562012b46b322af1f6f31af1
Fernando Cristiani <fer5cristiani@gmail.com>
2020-07-09 00:11:34 +0200

    Update calculator example

--- examples/calculator/calculator/index.js
+++ examples/calculator/calculator/index.js
@@ -1,5 +1,12 @@
-module.exports = function () {
-	this.sum = function (a, b) {
-		return a + b;
+module.exports = function (calculatorName) {
+	this.calculatorName = calculatorName;
+
+	this.sum = function (a, b, optionalParameter) {
+		var n = a + b;
+		if (optionalParameter && optionalParameter.hello && optionalParameter.world) {
+			n = n * 2;
+		}
+
+		return n;
 	};
 };
