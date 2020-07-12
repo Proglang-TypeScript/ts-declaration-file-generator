1d3e0f19005978dc9c292037e280fa6a8c8cccbb
Fernando Cristiani <fer5cristiani@gmail.com>
2020-07-12 11:41:43 +0200

    Make Module Writer extend from BaseWriter

--- src/TypescriptDeclaration/TypescriptDeclarationWriter/ModuleTypescriptDeclarationWriter.ts
+++ src/TypescriptDeclaration/TypescriptDeclarationWriter/ModuleTypescriptDeclarationWriter.ts
@@ -1,20 +1,20 @@
 import fs from 'fs';
-import { FunctionDeclaration } from "../FunctionDeclaration";
-import { ModuleTypescriptDeclaration } from "../ModuleDeclaration/ModuleTypescriptDeclaration";
-import { InterfaceAttributeDeclaration } from '../InterfaceDeclaration';
+import { BaseTypescriptDeclarationWriter } from './BaseTypescriptDeclarationWriter';
+import { BaseModuleTypescriptDeclaration } from '../ModuleDeclaration/BaseModuleTypescriptDeclaration';
 
-export class ModuleTypescriptDeclarationWriter {
-    write(typescriptModuleDeclaration: ModuleTypescriptDeclaration, outputDirectory: string) {
-        let filePath = outputDirectory + "/" + typescriptModuleDeclaration.module;
-        let fileName = filePath + "/index.d.ts";
-        this.cleanOutput(filePath, fileName);
-        
-        this.writeInterfaces(fileName, typescriptModuleDeclaration);
-        this.writeFunctions(fileName, typescriptModuleDeclaration);
-        this.writeClasses(fileName, typescriptModuleDeclaration);
+export class ModuleTypescriptDeclarationWriter extends BaseTypescriptDeclarationWriter {
+    doWrite(typescriptModuleDeclaration: BaseModuleTypescriptDeclaration) {
+        this.writeInterfaces(this.fileName, typescriptModuleDeclaration);
+        this.writeFunctions(this.fileName, typescriptModuleDeclaration);
+        this.writeClasses(this.fileName, typescriptModuleDeclaration);
     }
 
-    private writeInterfaces(fileName: string, typescriptModuleDeclaration: ModuleTypescriptDeclaration): void {
+
+    protected getExportedName(typescriptModuleDeclaration: BaseModuleTypescriptDeclaration): string {
+        return "";
+    }
+
+    private writeInterfaces(fileName: string, typescriptModuleDeclaration: BaseModuleTypescriptDeclaration): void {
         typescriptModuleDeclaration.interfaces.forEach(i => {
             fs.appendFileSync(
                 fileName,
@@ -43,7 +43,7 @@ export class ModuleTypescriptDeclarationWriter {
         });
     }
 
-    private writeFunctions(fileName: string, typescriptModuleDeclaration: ModuleTypescriptDeclaration): void {
+    private writeFunctions(fileName: string, typescriptModuleDeclaration: BaseModuleTypescriptDeclaration): void {
         typescriptModuleDeclaration.methods.forEach(functionDeclaration => {
             fs.appendFileSync(
                 fileName,
@@ -52,7 +52,7 @@ export class ModuleTypescriptDeclarationWriter {
         });
     }
 
-    private writeClasses(fileName: string, typescriptModuleDeclaration: ModuleTypescriptDeclaration): void {
+    private writeClasses(fileName: string, typescriptModuleDeclaration: BaseModuleTypescriptDeclaration): void {
         typescriptModuleDeclaration.classes.forEach(classDeclaration => {
                 fs.appendFileSync(
                     fileName,
@@ -75,55 +75,6 @@ export class ModuleTypescriptDeclarationWriter {
                 fileName,
                 "}\n\n"
             );
-        });
-    }
-
-    private getConstructorSignature(f: FunctionDeclaration) {
-        let argumentsWithType = this.buildArgumentsWithType(f);
-
-        return "constructor(" + argumentsWithType + ")";
-    }
-
-    private getFunctionNameWithTypes(f: FunctionDeclaration) {
-        let argumentsWithType = this.buildArgumentsWithType(f);
-
-        return `${f.name}(${argumentsWithType.join(", ")}): ${f.getReturnTypeOfs().join(" | ")}`;
-    }
-
-    private cleanOutput(filePath: string, fileName: string) : void {
-        if (!fs.existsSync(filePath)) {
-            fs.mkdirSync(filePath, { recursive: true });
-        }
-
-        if (fs.existsSync(fileName)) {
-            fs.unlinkSync(fileName);
-        }
-    }
-
-    private buildInterfaceAttribute(a: InterfaceAttributeDeclaration): string {
-        let types = a.type;
-        if (types.length > 1) {
-            types = types.filter(t => t !== "undefined");
-        }
-
-        return `'${a.name}'${a.optional ? "?" : ""}: ${types.join(" | ")}`;
-    }
-
-    private buildArgumentsWithType(f: FunctionDeclaration, mapping?: ((s: string) => string)): string[] {
-        return f.getArguments().map(argument => {
-            let argumentTypes = argument.getTypeOfs();
-            let colon = ":";
-            if (argument.isOptional()) {
-                if (argumentTypes.length > 1) {
-                    argumentTypes = argumentTypes.filter(t => t !== "undefined");
-                }
-
-                colon = "?:"
-            }
-
-            return `${argument.name}${colon} ${argumentTypes
-                .map(mapping || ((a: string) => a))
-                .join(" | ")}`;
         });
     }
 }
