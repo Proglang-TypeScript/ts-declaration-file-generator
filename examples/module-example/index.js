23749bcda8b50184668c6d5cffb2aacd599882c5
Fernando Cristiani <fer5cristiani@gmail.com>
2020-07-17 13:53:11 +0200

    Fix my-module example

--- examples/module-example/index.js
+++ examples/module-example/index.js
@@ -4,4 +4,5 @@ myModule.doSomething({ hello: "hello" });
 myModule.doSomething({hello: "hello"}, true);
 myModule.doSomething({world: "world"});
 myModule.doSomething({ world: "world" }, true);
+myModule.doSomething({ hello: "hello" }, false);
 myModule.doAnotherThing(123);
