5c829c3840758ca78c2e86bd2d26ba58c54f5cbf
Fernando Cristiani <fer5cristiani@gmail.com>
2020-09-08 23:28:02 +0200

    Classify index attributes also as string attributes

--- src/utils/InterfaceSubsetPrimitiveValidator.ts
+++ src/utils/InterfaceSubsetPrimitiveValidator.ts
@@ -4,8 +4,17 @@ export class InterfaceSubsetPrimitiveValidator {
 	private STRING_ATTRIBUTES = ['length'];
 
 	isInterfaceSubsetOfString(i: InterfaceDeclaration): boolean {
+		const attributes = i.getAttributes();
+
+		if (attributes.length === 0) return false;
+
 		return i.getAttributes().filter(
-			(a) => this.STRING_ATTRIBUTES.includes(a.name)
-		).length > 0;
+			(a) => !this.isStringAttribute(a.name)
+		).length === 0;
+	}
+
+	private isStringAttribute(attribute: string) {
+		return this.STRING_ATTRIBUTES.includes(attribute) ||
+			!isNaN(+attribute);
 	}
 }
--- tests/InterfaceSubsetPrimitiveValidator.test.ts
+++ tests/InterfaceSubsetPrimitiveValidator.test.ts
@@ -9,10 +9,56 @@ describe('InterfaceSubsetPrimitiveValidator', () => {
             ).toBe(false);
         });
 
+        it('should return false for an interface containing some non-string attributes', () => {
+            const stringInterface = new InterfaceDeclaration();
+            stringInterface.addAttribute({
+                name: 'hello',
+                type: ['number']
+            });
+
+            stringInterface.addAttribute({
+                name: 'world',
+                type: ['string']
+            });
+
+            expect(
+                new InterfaceSubsetPrimitiveValidator().isInterfaceSubsetOfString(stringInterface)
+            ).toBe(false);
+        });
+
+        it('should return false for an interface containing both string and non-string attributes', () => {
+            const stringInterface = new InterfaceDeclaration();
+            stringInterface.addAttribute({
+                name: 'length',
+                type: ['number']
+            });
+
+            stringInterface.addAttribute({
+                name: 'hello',
+                type: ['string']
+            });
+
+            expect(
+                new InterfaceSubsetPrimitiveValidator().isInterfaceSubsetOfString(stringInterface)
+            ).toBe(false);
+        });
+
         it('should return true for string attributes', () => {
             const stringInterface = new InterfaceDeclaration();
             stringInterface.addAttribute({
                 name: 'length',
+                type: ['number']
+            });
+
+            expect(
+                new InterfaceSubsetPrimitiveValidator().isInterfaceSubsetOfString(stringInterface)
+            ).toBe(true);
+        });
+
+        it('should return true for index access', () => {
+            const stringInterface = new InterfaceDeclaration();
+            stringInterface.addAttribute({
+                name: '3',
                 type: ['number']
             });
 
