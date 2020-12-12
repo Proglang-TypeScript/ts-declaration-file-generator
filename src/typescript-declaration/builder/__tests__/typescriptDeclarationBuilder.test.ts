94a5e6c9c6d61ee21dc7c3881b741e21c1bf4153
Fernando Cristiani <fer5cristiani@gmail.com>
2020-12-12 18:16:28 +0100

    Fix builder test

--- src/typescript-declaration/builder/__tests__/typescriptDeclarationBuilder.test.ts
+++ src/typescript-declaration/builder/__tests__/typescriptDeclarationBuilder.test.ts
@@ -1,18 +1,17 @@
 import { TypescriptDeclarationBuilder } from '../TypescriptDeclarationBuilder';
 import { RuntimeInfoParser } from '../../../runtime-info/parser/RunTimeInfoParser';
-import { FunctionDeclarationCleaner } from '../../../utils/FunctionDeclarationCleaner';
 
 describe('TypescriptDeclarationBuilder', () => {
   describe('optional parameters', () => {
     it('should mark the same argument as optional in all occurences of the function', () => {
-      const builder = new TypescriptDeclarationBuilder(new FunctionDeclarationCleaner());
-      builder.build(
+      const builder = new TypescriptDeclarationBuilder();
+      const dts = builder.build(
         new RuntimeInfoParser(`${__dirname}/files/optional-parameters/output.json`).parse(),
         'build-name',
       );
 
-      builder.functionDeclarations.forEach((f) => {
-        expect(f.getArguments()[1].isOptional()).toBe(true);
+      dts.functions?.forEach((f) => {
+        expect(f.parameters && f.parameters[1]?.optional).toBe(true);
       });
     });
   });
