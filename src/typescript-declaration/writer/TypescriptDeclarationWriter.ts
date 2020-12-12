2f50bb63335456b54d8a8e27bb1a68c702f7fbeb
Fernando Cristiani <fer5cristiani@gmail.com>
2020-12-12 16:11:52 +0100

    Delete unnecessary interface

--- src/typescript-declaration/writer/TypescriptDeclarationWriter.ts
+++
@@ -1,8 +0,0 @@
-import { BaseTemplateTypescriptDeclaration } from '../ModuleDeclaration/BaseTemplateTypescriptDeclaration';
-
-export interface TypescriptDeclarationWriter {
-  write(
-    typescriptModuleDeclaration: BaseTemplateTypescriptDeclaration,
-    outputDirectory: string,
-  ): string;
-}
